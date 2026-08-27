f7688d6f4f31a566973cdf5a048cd82355ec1e84
Keenan MARTIN <kesmartin2004@gmail.com>
2026-08-27 16:00:00 +0200

    Completer le LAB 02 et rendre archivage et contraintes verifiables

--- sql/00_pagila.sql
+++ sql/00_pagila.sql
@@ -1,9 +1,15 @@
 \set ON_ERROR_STOP on
 \connect blogapp_lab
+-- Le dump Pagila utilise postgres comme propriétaire, même si l'instance
+-- a été initialisée avec un autre superutilisateur.
+DO $$ BEGIN
+    IF NOT EXISTS (SELECT FROM pg_roles WHERE rolname = 'postgres') THEN
+        CREATE ROLE postgres NOLOGIN;
+    END IF;
+END $$;
 DROP DATABASE IF EXISTS pagila WITH (FORCE);
 CREATE DATABASE pagila OWNER labuser;
 \connect pagila
 \ir ../sources/pagila/pagila-schema.sql
 \ir ../sources/pagila/pagila-data.sql
 ANALYZE;
-
